0d9f80cf54e895c1b5938628e4c16cafdaa2601c
Денис Сиражитдинов <paradise6877@gmail.com>
2025-11-09 21:59:39 +0500

    Update task5.py

--- tasks/task5.py
+++ tasks/task5.py
@@ -4,7 +4,14 @@
 
 def solve():
 # Ниже пишите решение задачии(Обязательно поставьте четыре пробела после функции Solve())
+numbers = [2, 3, 4]
 
+squares = []
+for num in numbers:
+    squares.append(num * num)
+
+print("Числа:", numbers)
+print("Квадраты:", squares)
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
